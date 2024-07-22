c99f7df769518d5eadfcf1d365ef4f150c4c40e2
Joe Clapis <jclapis@outlook.com>
2024-07-22 10:08:10 -0400

    Updated exporter and Lodestar

--- config/lodestar-bn-config.go
+++ config/lodestar-bn-config.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	lodestarBnTagTest string = "chainsafe/lodestar:v1.20.0"
-	lodestarBnTagProd string = "chainsafe/lodestar:v1.20.0"
+	lodestarBnTagTest string = "chainsafe/lodestar:v1.20.2"
+	lodestarBnTagProd string = "chainsafe/lodestar:v1.20.2"
 )
 
 // Configuration for the Lodestar BN
