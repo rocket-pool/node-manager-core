74c95eff323a00e7e7e9acd6347bf8124d3de09a
Joe Clapis <jclapis@outlook.com>
2024-09-18 03:07:18 -0400

    Added logging to deposit data generation

--- node/validator/deposit-data.go
+++ node/validator/deposit-data.go
@@ -1,7 +1,9 @@
 package validator
 
 import (
+	"encoding/hex"
 	"fmt"
+	"log/slog"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/core/signing"
@@ -13,7 +15,7 @@ import (
 )
 
 // Get deposit data & root for a given validator key and withdrawal credentials
-func GetDepositData(validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials common.Hash, genesisForkVersion []byte, depositAmount uint64, networkName string) (beacon.ExtendedDepositData, error) {
+func GetDepositData(logger *slog.Logger, validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials common.Hash, genesisForkVersion []byte, depositAmount uint64, networkName string) (beacon.ExtendedDepositData, error) {
 	// Build deposit data
 	dd := ssz_types.DepositDataNoSignature{
 		PublicKey:             validatorKey.PublicKey().Marshal(),
@@ -54,9 +56,21 @@ func GetDepositData(validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials
 	if err != nil {
 		return beacon.ExtendedDepositData{}, err
 	}
+	if logger != nil {
+		logger.Debug("Created deposit data",
+			slog.String("pubkey", beacon.ValidatorPubkey(depositData.PublicKey).HexWithPrefix()),
+			slog.String("withdrawalCredentials", withdrawalCredentials.Hex()),
+			slog.Uint64("amount", depositAmount),
+			slog.String("signature", beacon.ValidatorSignature(depositData.Signature).HexWithPrefix()),
+			slog.String("domain", hex.EncodeToString(domain[:])),
+			slog.String("messageRoot", hex.EncodeToString(messageRoot[:])),
+			slog.String("signatureRoot", hex.EncodeToString(dataRootHash[:])),
+			slog.String("depositDataRoot", hex.EncodeToString(depositDataRoot[:])),
+		)
+	}
 
 	// Make sure everything is correct
-	err = ValidateDepositInfo(genesisForkVersion, depositAmount, dd.PublicKey, dd.WithdrawalCredentials, depositData.Signature)
+	err = ValidateDepositInfo(logger, genesisForkVersion, depositAmount, dd.PublicKey, dd.WithdrawalCredentials, depositData.Signature)
 	if err != nil {
 		return beacon.ExtendedDepositData{}, fmt.Errorf("deposit data failed signature validation: %w", err)
 	}
@@ -74,11 +88,16 @@ func GetDepositData(validatorKey *eth2types.BLSPrivateKey, withdrawalCredentials
 	}, nil
 }
 
-func ValidateDepositInfo(genesisForkVersion []byte, depositAmount uint64, pubkey []byte, withdrawalCredentials []byte, signature []byte) error {
+func ValidateDepositInfo(logger *slog.Logger, genesisForkVersion []byte, depositAmount uint64, pubkey []byte, withdrawalCredentials []byte, signature []byte) error {
 	// Get the deposit domain based on the eth2 config
 	depositDomain, err := signing.ComputeDomain(eth2types.DomainDeposit, genesisForkVersion, eth2types.ZeroGenesisValidatorsRoot)
 	if err != nil {
 		return err
+	}
+	if logger != nil {
+		logger.Debug("Validating deposit data",
+			slog.String("domain", hex.EncodeToString(depositDomain)),
+		)
 	}
 
 	// Create the deposit struct
